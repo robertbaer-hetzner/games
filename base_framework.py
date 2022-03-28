953239177f23b3f5b46a1587bf113c0b6bd6b3f3
robbae <robert@baerco.de>
2022-03-28 12:30:21 +0200

    init

---
+++ base_framework.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+#Github: https://github.com/robertbaer-hetzner
+#Autor: Robert Baer
+
+# Game : 
+
+# imports
+
+# classes 
+
+# functions
+
+#START
+if __name__ == "__main__":
+    print ("Hello World");
+
+
